c146a21585265840e75e35c04352bc42d0d031bf
Jakub Dąbek <jakub.dabek@gmail.com>
2018-10-09 20:48:29 +0200

    Change greeting to something more movie-quoty

--- src/main/java/pl/dabek/jakub/tpl1/HelloGreeting.java
+++ src/main/java/pl/dabek/jakub/tpl1/HelloGreeting.java
@@ -2,7 +2,7 @@ package pl.dabek.jakub.tpl1;
 
 public final class HelloGreeting implements Greeting {
 
-    public static final String GREETING = "Hello ";
+    public static final String GREETING = "Greetings and salutations ";
 
     public String sayGreeting(final String name) {
         return GREETING + (name != null ? name : DEFAULT_NAME) + "!";
