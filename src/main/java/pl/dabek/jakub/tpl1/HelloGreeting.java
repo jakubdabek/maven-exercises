3b51c7515a1b45156c560d023d3adff9e347b1a3
Kamil Król <33430953+KamilKrol5@users.noreply.github.com>
2018-10-09 20:44:55 +0200

    Change greeting to "Witaj"

--- src/main/java/pl/dabek/jakub/tpl1/HelloGreeting.java
+++ src/main/java/pl/dabek/jakub/tpl1/HelloGreeting.java
@@ -2,7 +2,7 @@ package pl.dabek.jakub.tpl1;
 
 public final class HelloGreeting implements Greeting {
 
-    public static final String GREETING = "Hello ";
+    public static final String GREETING = "Witaj ";
 
     public String sayGreeting(final String name) {
         return GREETING + (name != null ? name : DEFAULT_NAME) + "!";
